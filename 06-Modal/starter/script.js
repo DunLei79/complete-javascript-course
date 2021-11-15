587d86d4ff1482f068a665fe60de8b4ce280439f
Duncan Leite <duncanleite@yahoo.com>
2021-11-15 14:24:27 +0200

    study

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,16 +6,31 @@ const btnCloseModal = document.querySelector(`.close-modal`);
 const btnsOpenModal = document.querySelectorAll(`.show-modal`);
 
 
-for(let i = 0; i < btnsOpenModal.length; i++){
-btnsOpenModal[i].addEventListener(`click`, function(){
+const openModal =function(){
     console.log(`button clicked`)
     modal.classList.remove(`hidden`);
     overlay.classList.remove(`hidden`);
-})
 };
 
-btnCloseModal.addEventListener(`click`,function(){
+for(let i = 0; i < btnsOpenModal.length; i++){
+btnsOpenModal[i].addEventListener(`click`, openModal)
+};
+
+const closeModal =function (){
     modal.classList.add(`hidden`);
     overlay.classList.add(`hidden`);
+};
+
+btnCloseModal.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`,closeModal);
+
+document.addEventListener(`keydown`,function(e){
+   console.log(e.key); 
+
+   if(e.key === `Escape` && !modal.classList.contains(`hidden`)){
+           closeModal();
+       
+   };
 })
+
 
