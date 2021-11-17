926f3e8420e5da3dea92e6dcbdc0aa28cd055c3d
Duncan Leite <duncanleite@yahoo.com>
2021-11-17 15:39:41 +0200

    study

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+console.log(this);
+
+const calcAge = function(birthYear) {
+    console.log(2037-birthYear);
+    console.log(this);
+};
+  
+calcAge(1991);
+
+const calcAgeArrow = birthYear=> {
+    console.log(2037-birthYear);
+    console.log(this);
+};
+  
+calcAgeArrow(1991);
+
+const duncan = {
+    year: 1991,
+    calcAge: function(){
+        console.log(this);
+    },
+};
+duncan.calcAge();
